d7eb5c4c163c4aeb20b172d78c4269a175d7f779
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-24 09:17:47 +0200

    update step-2

--- src/components/CounterFeedback/CounterFeedback.jsx
+++ src/components/CounterFeedback/CounterFeedback.jsx
@@ -16,16 +16,37 @@ class CounterFeedback extends React.Component {
     goodValue: 0,
     neutralValue: 0,
     badValue: 0,
+    totalValue: 0,
+    positivePercentageValue: 0,
   };
 
   handleIncrementGood = () => {
     this.setState(prevState => ({ goodValue: prevState.goodValue + 1 }));
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
   };
   handleIncrementNeutral = () => {
     this.setState(prevState => ({ neutralValue: prevState.neutralValue + 1 }));
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
   };
   handleIncrementBad = () => {
     this.setState(prevState => ({ badValue: prevState.badValue + 1 }));
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+  countTotalFeedback = () => {
+    this.setState(prevState => ({
+      totalValue:
+        prevState.goodValue + prevState.neutralValue + prevState.badValue,
+    }));
+  };
+  countPositiveFeedbackPercentage = () => {
+    this.setState(prevState => ({
+      positivePercentageValue: Math.round(
+        (prevState.goodValue / prevState.totalValue) * 100
+      ),
+    }));
   };
 
   render() {
@@ -57,6 +78,15 @@ class CounterFeedback extends React.Component {
             <StatisticsItem>
               Bad: <StatisticsSpan>{this.state.badValue}</StatisticsSpan>
             </StatisticsItem>
+            <StatisticsItem>
+              Total: <StatisticsSpan>{this.state.totalValue}</StatisticsSpan>
+            </StatisticsItem>
+            <StatisticsItem>
+              Positive feedback:{' '}
+              <StatisticsSpan>
+                {this.state.positivePercentageValue}%
+              </StatisticsSpan>
+            </StatisticsItem>
           </StatisticsList>
         </StatisticsWrapp>
       </>
@@ -64,31 +94,4 @@ class CounterFeedback extends React.Component {
   }
 }
 
-// const CounterFeedback = () => {
-//   return (
-//     <>
-//       <FeedbackWrapp>
-//         <FeedbackTitle>Please leave feedback</FeedbackTitle>
-//         <Button>Good</Button>
-//         <Button>Neutral</Button>
-//         <Button>Bad</Button>
-//       </FeedbackWrapp>
-
-//       <StatisticsWrapp>
-//         <StatisticsTitle>Statistics</StatisticsTitle>
-//         <StatisticsList>
-//           <StatisticsItem>
-//             Good: <StatisticsSpan>0</StatisticsSpan>
-//           </StatisticsItem>
-//           <StatisticsItem>
-//             Neutral: <StatisticsSpan>0</StatisticsSpan>
-//           </StatisticsItem>
-//           <StatisticsItem>
-//             Bad: <StatisticsSpan>0</StatisticsSpan>
-//           </StatisticsItem>
-//         </StatisticsList>
-//       </StatisticsWrapp>
-//     </>
-//   );
-// };
 export default CounterFeedback;
--- src/components/CounterFeedback/CounterFeedback.stuled.js
+++ src/components/CounterFeedback/CounterFeedback.stuled.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 export const FeedbackWrapp = styled.div`
+  box-sizing: border-box;
   margin-left: auto;
   margin-right: auto;
   margin-top: 100px;
   padding: 40px;
-  width: 300px;
+  width: 400px;
   height: 200px;
   text-align: center;
-  background-color: blue;
+  background-color: #00B0EA;
   box-shadow: -1px -1px 31px 0px rgba(0, 0, 0, 0.75);
+  border-radius: 5px 5px 0 0;
 `;
 
 export const FeedbackTitle = styled.h1`
@@ -28,22 +30,31 @@ export const Button = styled.button`
 `;
 
 export const StatisticsWrapp = styled.div`
-  width: 300px;
+  box-sizing: border-box;
+  width: 400px;
   height: auto;
   margin-left: auto;
   margin-right: auto;
   padding: 30px;
   text-align: center;
   background-color: aqua;
+  box-shadow: -1px 5px 31px 0px rgba(0, 0, 0, 0.75);
+  border-radius: 0 0 5px 5px;
 `;
 export const StatisticsTitle = styled.h2`
-margin: 0;
+  margin: 0;
 `;
 export const StatisticsList = styled.ul`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-evenly;
+  row-gap: 20px;
+  column-gap: 10px;
   list-style: none;
   padding: 0;
 `;
 export const StatisticsItem = styled.li``;
-export const StatisticsSpan = styled.span``;
+export const StatisticsSpan = styled.span`
+  font-size: 22px;
+  font-weight: 500;
+`;
